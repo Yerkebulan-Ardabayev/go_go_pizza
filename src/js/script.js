a8b26bc8578436abfbf5ffa901627b56c6e4215b
Yerkebulan Ardabayev <yerkebulan.ardabayev@gmail.com>
2024-06-18 17:34:01 +0500

    js

--- src/js/script.js
+++ src/js/script.js
@@ -19,9 +19,74 @@ const toppingsToggle = () => {
   });
 };
 
+const getPizzas = async () => {
+  try {
+    const response = await fetch(
+      'https://well-zest-library.glitch.me/api/products'
+    );
+    if (!response.ok) {
+      throw new Error('Failed you fetch!!!!');
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(`Error fetching pizza: ${error}`);
+  }
+};
+
+const createCard = (data) => {
+  const card = document.createElement('article');
+  card.classList.add('card', 'pizza__card');
+
+  card.innerHTML = `
+  <picture>
+  <source srcset="${data.images[1]}" type="image/webp">
+  <img
+    class="card__image"
+    src="${data.images[0]}"
+    alt="${data.name.ru}" />
+  </picture>
+  
+      <div class="card__content">
+        <h3 class="card__title">${data.name['ru'][0].toUpperCase()}${data.name[
+    'ru'
+  ]
+    .slice(1)
+    .toLowerCase()}</h3>
+          <p class="card__info">
+            <span class="card__price">${data.price['25cm']} тенге</span>
+              <span>/</span>
+                <span class="card__weight">25 см</span>
+          </p>
+
+            <button class="card__button" data-id="${data.id}">Выбрать</button>
+      </div>
+  `;
+  return card;
+};
+
+const renderPizzas = async () => {
+  const pizzas = await getPizzas();
+  const pizzaList = document.querySelector('.pizza__list');
+  pizzaList.textContent = '';
+
+  const items = pizzas.map((data) => {
+    const item = document.createElement('li');
+    item.classList.add('pizza__item');
+
+    const card = createCard(data);
+    item.append(card);
+
+    return item;
+  });
+
+  pizzaList.append(...items);
+};
 
 const init = () => {
   toppingsToggle();
+
+  renderPizzas();
 };
 
 init();
